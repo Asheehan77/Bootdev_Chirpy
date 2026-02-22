64ced7b8fa3f9d6d1886e3a4ceb0cda06de5dd21
Asheehan77 <anthony.sheehan77@gmail.com>
2026-02-22 01:10:39 -0500

    added index file for server

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
-import(
+import (
 	"net/http"
 )
 
-func main(){
+func main() {
 	server_mux := http.NewServeMux()
+	handler := http.FileServer(http.Dir("."))
+
+	server_mux.Handle("/", handler)
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: server_mux,
 	}
 
 	server.ListenAndServe()
 
-	return
 }
